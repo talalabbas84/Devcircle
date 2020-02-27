7760639189d3b5feef83a16160b0ab7282a7c5c8
Talal Abbas <talalabbas84@gmail.com>
2020-02-28 01:53:50 +0500

    React User Authhentication

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,11 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 //import axios from 'axios';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -15,27 +18,32 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    //else {
-    //   const newUser = {
-    //     name,
-    //     email,
-    //     password
-    //   };
-    //   try {
-    //     const config = {
-    //       headers: {
-    //         'Content-Type': 'application/json'
-    //       }
-    //     };
-    //     const body = JSON.stringify(newUser);
-    //     const res = await axios.post('/api/users', body, config);
-    //     console.log(res.data);
-    console.log('success');
-    //   } catch (err) {
-    //     console.log(err.response.data);
-    //   }
+    login(email, password);
   };
-
+  //else {
+  //   const newUser = {
+  //     name,
+  //     email,
+  //     password
+  //   };
+  //   try {
+  //     const config = {
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     };
+  //     const body = JSON.stringify(newUser);
+  //     const res = await axios.post('/api/users', body, config);
+  //     console.log(res.data);
+  // console.log('success');
+  //   } catch (err) {
+  //     console.log(err.response.data);
+  //   }
+  //login(email, password);
+  // Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign In</h1>
@@ -73,4 +81,12 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps, { login })(Login);
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 //import axios from 'axios';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -38,12 +39,16 @@ const Register = ({ setAlert }) => {
       //     const body = JSON.stringify(newUser);
       //     const res = await axios.post('/api/users', body, config);
       //     console.log(res.data);
-      console.log('success');
+      // console.log('success');
       //   } catch (err) {
       //     console.log(err.response.data);
       //   }
+      register({ name, email, password });
     }
   };
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
@@ -58,7 +63,7 @@ const Register = ({ setAlert }) => {
             name='name'
             value={name}
             onChange={e => onChange(e)}
-            required
+            // required
           />
         </div>
         <div className='form-group'>
@@ -68,7 +73,7 @@ const Register = ({ setAlert }) => {
             name='email'
             value={email}
             onChange={e => onChange(e)}
-            required
+            // required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -82,7 +87,7 @@ const Register = ({ setAlert }) => {
             name='password'
             value={password}
             onChange={e => onChange(e)}
-            minLength='6'
+            // minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -92,7 +97,7 @@ const Register = ({ setAlert }) => {
             name='password2'
             value={password2}
             onChange={e => onChange(e)}
-            minLength='6'
+            // minLength='6'
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
@@ -104,7 +109,12 @@ const Register = ({ setAlert }) => {
   );
 };
 Register.propType = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
-export default connect(null, { setAlert })(Register);
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps, { setAlert, register })(Register);
