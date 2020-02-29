22eefb7cd23d2dec0fcb0db5ad3cdf95b4dac193
Talal Abbas <talalabbas84@gmail.com>
2020-02-29 14:25:20 +0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 var bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 //Connect Database
@@ -11,13 +12,21 @@ connectDB();
 app.use(bodyParser.json());
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
