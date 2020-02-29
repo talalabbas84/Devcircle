c430f83ed33efbe35fc282795bb6e3940ce1e672
Talal Abbas <talalabbas84@gmail.com>
2020-02-29 14:55:54 +0500

    fixed bug deployment

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-var bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
@@ -9,7 +8,6 @@ connectDB();
 
 //Init Middleware
 
-app.use(bodyParser.json());
 app.use(express.json({ extended: false }));
 
 // Define Routes
